4903f29de1883e2cd9e16d4425c90a5909ae020b
nihilous <dkfg1@naver.com>
2024-08-05 22:36:47 +0300

    GCP setting, trouble shooting, browser test done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,20 +3,24 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import userRouter from './controllers/userController';
 import runMigrations from './runMigration';
-import { checkDatabaseConnection } from './db'
+import { checkDatabaseConnection } from './db';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/user', userRouter);
+app.use('/ser', userRouter);
 
-app.get('/', (req, res) => {
-    res.send('setup done');
+app.get('/api/', (req, res) => {
+    res.send('API is working');
+});
+
+app.get('/api/test', (req, res) => {
+    res.send('Test endpoint');
 });
 
 async function startServer() {
@@ -25,8 +29,8 @@ async function startServer() {
 
         await runMigrations();
 
-        app.listen(port, () => {
-            console.log(`Server is running at http://localhost:${port}`);
+        app.listen(port, '0.0.0.0', () => {
+            console.log(`Server is running at http://0.0.0.0:${port}`);
         });
     } catch (error) {
         console.error('Error starting server:', error);
