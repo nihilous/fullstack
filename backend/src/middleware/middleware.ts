96205ebe6dfb7a01a5b8bd1c6149236831f28b87
nihilous <dkfg1@naver.com>
2024-09-12 13:33:32 +0300

    Backend date type check return value bugfix

--- backend/src/middleware/middleware.ts
+++ backend/src/middleware/middleware.ts
@@ -200,7 +200,7 @@ function isNotNumber(inputs: any[]): boolean {
 
 function isDateFormat(input: string): boolean {
     const regex = /^\d{4}-\d{2}-\d{2}$/;
-    return regex.test(input);
+    return !regex.test(input);
 }
 
 export {CustomRequest, tokenExtractor, isNotNumber, injectionChecker, patternChecker, isDateFormat};
