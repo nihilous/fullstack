b7e8da0261ad654238cfab81cfd2293647e8ceb8
nihilous <dkfg1@naver.com>
2024-09-16 17:11:28 +0300

    github action flow test and deploy to gcp test 4

--- frontend/src/components/LoginJoin.tsx
+++ frontend/src/components/LoginJoin.tsx
@@ -141,7 +141,7 @@ const LoginJoin = () => {
                     :
                     <Row className="justify-content-md-center">
                         <div>
-                            github action flow test and deploy to gcp test 3
+                            github action flow test and deploy to gcp test 4
                         </div>
                         <Col md={6}>
                             {isLoginView ? (
