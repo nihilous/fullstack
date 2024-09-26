32f2e891282ec4cc3f16587151ef56239194d394
nihilous <dkfg1@naver.com>
2024-09-26 03:36:26 +0300

    Prod Build Deploy Test 19

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
